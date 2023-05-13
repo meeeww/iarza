e84391204c972668edf77beb2d921958aec98b5e
meeeww <thezasm03@gmail.com>
2023-05-13 02:58:09 +0200

    Updated header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,21 +1,21 @@
 function Header() {
     return (
-        <header>
+        <header className="headerMain shadow-2xl">
             <div className="flex flex-col justify-center bg-[var(--colorPrincipal)] py-3 md:flex-row md:justify-around">
                 <div className="flex justify-center md:justify-around">
-                    <h2>Facebook</h2>
-                    <h2>YouTube</h2>
+                    <a href="#"><i className="fa-brands fa-facebook-f text-[1.2rem] font-bold text-[var(--colorBlanco)] hover:text-[var(--colorGris)] transition-all"></i></a>
+                    <a href="#"><i className="fa-brands fa-youtube text-[1.2rem] font-bold text-[var(--colorBlanco)] hover:text-[var(--colorGris)] transition-all"></i></a>
                 </div>
                 <div className="flex justify-center md:justify-around">
-                    <h2>Telefono</h2>
-                    <h2>Correo</h2>
-                    <h2>Localidad</h2>
+                    <a href="#" className="text-[1rem] text-[var(--colorBlanco)] hover:text-[var(--colorGris)] transition-all"><i className="fa-solid fa-phone"></i>+34 982 20 14 28 </a>
+                    <a href="#" className="text-[1rem] text-[var(--colorBlanco)] hover:text-[var(--colorGris)] transition-all"><i className="fa-solid fa-envelope"></i>info.arza@iarza.com </a>
+                    <a href="#" className="text-[1rem] text-[var(--colorBlanco)] hover:text-[var(--colorGris)] transition-all"><i className="fa-solid fa-location-dot"></i>Lugo, Galicia</a>
                 </div>
             </div>
             <div className="flex flex-col justify-center mx-[5rem] py-4 md:justify-between md:flex-row">
                 <div className="flex justify-center md:justify-around">
-                    <h1>Logo</h1>
-                    <h1>Segundo Logo</h1>
+                    <a href="#">Logo</a>
+                    <a href="#">Segundo Logo</a>
                 </div>
                 <div className="flex justify-center md:justify-around">
                     <h2>Principal</h2>
