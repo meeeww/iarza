efeb91118afb02c86020f939cd708c0a02ec5e26
meeeww <thezasm03@gmail.com>
2023-07-31 22:33:40 +0200

    Fix de responsive

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -41,7 +41,7 @@ function Footer() {
     return (
         <footer className="pt-10 bg-[color:var(--colorGris)]">
             <div className="mx-auto px-4 md:px-16">
-                <div className="flex-1 mx-20 space-y-6 justify-around sm:flex md:space-y-0">
+                <div className="flex-1 mx-20 space-y-6 justify-around sm:flex md:space-y-0 text-center sm:text-left">
                     {
                         footerNavs.map((item, idx) => (
                             <ul
@@ -69,11 +69,11 @@ function Footer() {
                     }
                 </div>
                 <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
-                    <div className="flex flex-col mx-20 justify-center text-[color:var(--colorBlanco)]">
+                    <div className="flex flex-col mx-20 justify-center text-[color:var(--colorBlanco)] text-center sm:text-left">
                         <p>©2023 Ingeniería Arza S.L.</p>
                         <p>Todos los derechos reservados.</p>
                     </div>
-                    <div className="flex items-center mx-20 gap-x-6 text-[color:var(--colorBlanco)] mt-6">
+                    <div className="flex mx-20 items-center gap-x-6 text-[color:var(--colorBlanco)] mt-6 text-center sm:justify-center justify-evenly">
                         <a href="www.facebook.com/people/Ingeniería-Arza/100063934699829/" target="_blank">
                             <i className="fa-brands fa-facebook hover:text-[color:var(--colorPrincipal)] text-2xl"></i>
                         </a>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -33,17 +33,17 @@ function Inicio() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto w-full lg:mx-0 lg:max-w-none flex items-center justify-center">
             <div>
-              <div className="w-[60rem]">
+              <div className="lg:w-[60rem] w-[23rem] sm:w-[30rem] lg:block flex flex-col justify-center">
                 <p className="mt-2 text-3xl font-bold tracking-tight text-[color:var(--colorBlanco)] sm:text-4xl">Nuestros Servicios</p>
                 <p className="mt-6 text-lg leading-8 text-[color:var(--colorBlanco)]">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                   iste dolor cupiditate blanditiis ratione.
                 </p>
-                <dl className="mt-10 max-w-xl text-base leading-7 text-[color:var(--colorBlanco)] lg:max-w-none flex">
+                <dl className="mt-10 text-base leading-7 text-[color:var(--colorBlanco)] lg:max-w-none sm:flex">
                   {features.map((feature) => (
-                    <div key={feature.name} className="relative pl-9">
-                      <dt className="inline font-semibold text-[color:var(--colorBlanco)]">
-                        <feature.icon className="absolute left-1 h-5 w-5 text-[var(--colorPrincipal)]" aria-hidden="true" />
+                    <div key={feature.name} className="relative sm:my-0 mt-4">
+                      <dt className="inline font-semibold text-[color:var(--colorBlanco)] pl-9">
+                        <feature.icon className="absolute left-1 h-5 w-5 text-[var(--colorPrincipal)]"/>
                         {feature.name}
                       </dt>{' '}
                       <dd className="inline">{feature.description}</dd>
@@ -59,9 +59,9 @@ function Inicio() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto w-full lg:mx-0 lg:max-w-none flex items-center justify-center">
             <div>
-              <div className="w-[60rem]">
+              <div className="lg:w-[60rem] w-[23rem] sm:w-[30rem] lg:block flex flex-col justify-center">
                 <p className="mt-2 text-3xl font-bold tracking-tight text-[color:var(--colorGris)] sm:text-4xl">Visítanos En Nuestra Oficina</p>
-                <div className='flex items-center my-8'>
+                <div className='flex sm:flex-row flex-col items-center my-8'>
                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.6024177678537!2d-7.552206287870309!3d43.0077056938189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd31ce8c09ec3391%3A0x8c15da6c6ce4749d!2sIngenier%C3%ADa%20Arza%20S.L.!5e0!3m2!1ses!2ses!4v1690831545740!5m2!1ses!2ses" width="600" height="300" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                   <p className="mt-6 text-lg leading-8 text-[color:var(--colorGris)] mx-8">
                     Si tienes algún proyecto en mente o quieres realizarnos cualquier consulta, no dudes en ponerte en contacto con nosotros.
