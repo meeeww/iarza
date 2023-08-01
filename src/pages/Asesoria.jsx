bcf20367c673552304da0d280fac8953a68badc2
meeeww <thezasm03@gmail.com>
2023-08-01 20:55:38 +0200

    Añadidos iconos a la página de asesoria

--- src/pages/Asesoria.jsx
+++ src/pages/Asesoria.jsx
@@ -3,33 +3,27 @@ import Breadcrumb from '../componentes/Breadcrumb'
 
 import Visitanos from "../componentes/Visitanos";
 
-import {
-    DocumentTextIcon,
-    CubeTransparentIcon,
-    BriefcaseIcon
-} from '@heroicons/react/20/solid'
-
 const features = [
     {
         name: 'Laboral.',
         description:
             'Somos especialistas en gestión laboral, brindando servicios ágiles y precisos en tramitación de ayudas y prestaciones, elaboración minuciosa de contratos y nóminas, seguimiento meticuloso de altas en Seguridad Social, asesoramiento experto en Recursos Humanos y asistencia legal confiable para inspecciones, garantizando tranquilidad y cumplimiento normativo.',
-        icon: DocumentTextIcon,
+        icon: "network-wired",
     },
     {
         name: 'Agrícola.',
         description: 'Te ofrecemos un servicio integral con estudio personalizado de explotación para máxima rentabilidad, matriculación de maquinaria, cambios de titularidad y transferencias de ayudas PAC, y modificaciones catastrales y cambios de uso SIXPAC.',
-        icon: CubeTransparentIcon,
+        icon: "leaf",
     },
     {
         name: 'Fiscal.',
         description: 'Ofrecemos gestiones con la Agencia Tributaria, incluyendo Declaraciones de la Renta, Impuesto de Sociedades, IVA, contabilidad para empresas, constitución de sociedades y tramitación de impuesto de sucesiones. También gestionamos trámites catastrales y brindamos asistencia en inspecciones fiscales, con eficiente planificación tributaria.',
-        icon: BriefcaseIcon,
+        icon: "gavel",
     },
     {
         name: 'Jurídica.',
         description: 'Brindamos servicios legales integrales, incluyendo la elaboración de contratos, asistencia en litigios judiciales y procedimientos contencioso-administrativos. Además, proporcionamos asesoramiento especializado en procesos sancionadores por infracción de normativa medioambiental, así como en cuestiones de derechos de propiedad y acceso al Registro de la Propiedad de inmuebles.',
-        icon: BriefcaseIcon,
+        icon: "briefcase",
     },
 ]
 
@@ -44,13 +38,13 @@ function Asesoria() {
                             <div className="lg:w-[60rem] w-[23rem] sm:w-[30rem] lg:block flex flex-col justify-center">
                                 <p className="mt-2 text-3xl font-bold tracking-tight text-[color:var(--colorBlanco)] sm:text-4xl">Nuestras Asesorías</p>
                                 <p className="mt-6 text-lg leading-8 text-[color:var(--colorBlanco)]">
-                                Estudio integral, personalizado y detallado de la explotación para asesorarte y obtener la máxima rentabilidad de tu actividad, modernizando tus instalaciones, reduciendo costes y mejorando la productividad.
+                                    Estudio integral, personalizado y detallado de la explotación para asesorarte y obtener la máxima rentabilidad de tu actividad, modernizando tus instalaciones, reduciendo costes y mejorando la productividad.
                                 </p>
                                 <dl className="mt-10 text-base leading-7 text-[color:var(--colorBlanco)] lg:max-w-none sm:grid grid-cols-2 gap-8">
                                     {features.map((feature) => (
                                         <div key={feature.name} className="relative sm:my-0 mt-4">
                                             <dt className="inline font-semibold text-[color:var(--colorBlanco)] pl-6">
-                                                <feature.icon className="absolute left-0 h-5 w-5 top-1 text-[var(--colorPrincipal)]" />
+                                                <i className={"text-center absolute left-0 h-5 w-5 top-1 text-[var(--colorPrincipal)] fa-solid fa-" + feature.icon} />
                                                 {feature.name}
                                             </dt>{' '}
                                             <dd className="inline">{feature.description}</dd>
